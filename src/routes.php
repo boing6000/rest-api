86946a71c051a20a3f28c78e2b0051746b538ea4
Andre Teodoro <boing6000@hotmail.com>
2018-02-19 14:02:39 -0300

    Add files via upload

--- src/routes.php
+++ src/routes.php
@@ -15,9 +15,10 @@ function genRoutes($baseName = '/') {
 			Route::delete('logout', "$authController@logout");
 			Route::get('ad', "$authController@activeDirectory");
 		});
-			
-		foreach (getConfig('genericRoutes') as $key => $group) {
-			parseGroup($group);
+		if(count(getConfig('genericRoutes')) > 0){
+			foreach (getConfig('genericRoutes') as $key => $group) {
+				parseGroup($group);
+			}
 		}
 	});
 }
